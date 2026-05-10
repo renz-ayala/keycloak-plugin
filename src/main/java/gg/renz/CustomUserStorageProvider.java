887bce589bf887116ee4601a2ce6b201075b03c1
renz.ayala <renzayala98@gmail.com>
2026-05-09 22:04:12 -0500

    fist commit

---
+++ src/main/java/gg/renz/CustomUserStorageProvider.java
@@ -0,0 +1,153 @@
+package gg.renz;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.CredentialInputValidator;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.RealmModel;
+import org.keycloak.models.SubjectCredentialManager;
+import org.keycloak.models.UserModel;
+import org.keycloak.models.credential.PasswordCredentialModel;
+import org.keycloak.storage.UserStorageProvider;
+import org.keycloak.storage.adapter.AbstractUserAdapter;
+import org.keycloak.storage.user.UserLookupProvider;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CustomUserStorageProvider
+        implements UserStorageProvider,
+        UserLookupProvider,
+        CredentialInputValidator {
+
+    private final KeycloakSession session;
+    private final ComponentModel model;
+
+    public CustomUserStorageProvider(
+            KeycloakSession session,
+            ComponentModel model) {
+
+        this.session = session;
+        this.model = model;
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public UserModel getUserById(
+            RealmModel realm,
+            String id) {
+
+        //return getUserByUsername(realm, id);
+        return null;
+    }
+
+    @Override
+    public UserModel getUserByUsername(
+            RealmModel realm,
+            String username) {
+
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@host.docker.internal:1521/XE",
+                "SYSTEM",
+                "Greg1234.Database")) {
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT username FROM EXTRANET.USUARIOS WHERE username = ?"
+            );
+
+            statement.setString(1, username);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                return new AbstractUserAdapter(session, realm, model) {
+
+                    @Override
+                    public String getUsername() {
+                        return username;
+                    }
+
+                    @Override
+                    public SubjectCredentialManager credentialManager() {
+                        return null;
+                    }
+                };
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public UserModel getUserByEmail(
+            RealmModel realm,
+            String email) {
+
+        return null;
+    }
+
+    @Override
+    public boolean supportsCredentialType(String credentialType) {
+
+        return PasswordCredentialModel.TYPE.equals(credentialType);
+    }
+
+    @Override
+    public boolean isConfiguredFor(
+            RealmModel realm,
+            UserModel user,
+            String credentialType) {
+
+        return supportsCredentialType(credentialType);
+    }
+
+    @Override
+    public boolean isValid(
+            RealmModel realm,
+            UserModel user,
+            CredentialInput input) {
+
+        if (!supportsCredentialType(input.getType())) {
+            return false;
+        }
+
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@host.docker.internal:1521/XE",
+                "SYSTEM",
+                "Greg1234.Database")) {
+
+            PreparedStatement statement = connection.prepareStatement(
+                    "SELECT password FROM EXTRANET.USUARIOS WHERE username = ?"
+            );
+
+            statement.setString(1, user.getUsername());
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+
+                String dbPassword = resultSet.getString("password");
+
+                return dbPassword.equals(
+                        input.getChallengeResponse()
+                );
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/gg/renz/CustomUserStorageProviderFactory.java
@@ -0,0 +1,22 @@
+package gg.renz;
+
+import org.keycloak.component.ComponentModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.storage.UserStorageProviderFactory;
+
+public class CustomUserStorageProviderFactory
+        implements UserStorageProviderFactory<CustomUserStorageProvider> {
+
+    @Override
+    public String getId() {
+        return "oracle-storage-gg";
+    }
+
+    @Override
+    public CustomUserStorageProvider create(
+            KeycloakSession session,
+            ComponentModel model) {
+
+        return new CustomUserStorageProvider(session, model);
+    }
+}
