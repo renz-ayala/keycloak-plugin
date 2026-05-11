4ba6968fc599c4b60302ddfc20f9bac277d560c9
rayalaj <rayala@sunarp.gob.pe>
2026-05-11 15:40:35 -0500

    mejoras para la coneccion desde docker a oracle.

--- src/main/java/gg/renz/CustomUserStorageProvider.java
+++ src/main/java/gg/renz/CustomUserStorageProvider.java
@@ -17,83 +17,164 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class CustomUserStorageProvider
-        implements UserStorageProvider,
-        UserLookupProvider,
-        CredentialInputValidator {
+public class CustomUserStorageProvider implements UserStorageProvider, UserLookupProvider, CredentialInputValidator
+{
+    private final KeycloakSession _session;
+    private final ComponentModel _model;
 
-    private final KeycloakSession session;
-    private final ComponentModel model;
+    String _db_url = "jdbc:oracle:thin:@172.18.1.73:1521/DESABODEGA";
+    String _db_username = "user1";
+    String _db_password = "user1";
 
-    public CustomUserStorageProvider(
-            KeycloakSession session,
-            ComponentModel model) {
-
-        this.session = session;
-        this.model = model;
+    public CustomUserStorageProvider(KeycloakSession session, ComponentModel model)
+    {
+        this._session = session;
+        this._model = model;
     }
 
     @Override
-    public void close() {
-
+    public void close()
+    {
     }
 
     @Override
-    public UserModel getUserById(
-            RealmModel realm,
-            String id) {
-
+    public UserModel getUserById(RealmModel realm, String id)
+    {
         //return getUserByUsername(realm, id);
         return null;
     }
 
     @Override
-    public UserModel getUserByUsername(
-            RealmModel realm,
-            String username) {
+    public UserModel getUserByUsername(RealmModel realm, String username)
+    {
 
         System.out.println("!!! INTENTANDO BUSCAR EN ORACLE A: " + username);
 
-        try { Class.forName("oracle.jdbc.OracleDriver"); } catch (Exception e) {}
+        try
+        {
+            Class.forName("oracle.jdbc.OracleDriver");
+        } catch (Exception e)
+        {
+            System.out.println("FALLA EN EL CLASS FORNAME");
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
 
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:oracle:thin:@192.168.1.4:1521/XE",
-                "SYSTEM",
-                "Greg1234.Database")) {
+        System.out.println("DB_URL: " + _db_url);
+        System.out.println("DB_USER: " + _db_username);
+        System.out.println("INTENTANDO CONEXION...");
+
+        try (Connection connection = DriverManager.getConnection(_db_url, _db_username, _db_password))
+        {
+            System.out.println("LLEGO AL QUERY");
 
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT username FROM EXTRANET.USUARIOS WHERE username = ?"
+                    "SELECT username, nombres, apellidos, correo, activo FROM user1.sso_users WHERE username = ?"
             );
 
             statement.setString(1, username);
-
             ResultSet resultSet = statement.executeQuery();
 
-            if (resultSet.next()) {
-                System.out.println("!!! USUARIO ENCONTRADO EN DB: " + username);
-                final String finalUsername = username;
+            if (resultSet.next())
+            {
+                String nombres = resultSet.getString("nombres");
+                String apellidos = resultSet.getString("apellidos");
+                String correo = resultSet.getString("correo");
+                int activo = resultSet.getInt("activo");
 
-                AbstractUserAdapter adapter = new AbstractUserAdapter(session, realm, model) {
+                System.out.println("!!! USUARIO ENCONTRADO EN DB: " +  username);
+
+                final String finalUsername = username;
+                final String finalNombres = nombres;
+                final String finalApellidos = apellidos;
+                final String finalCorreo = correo;
+                final boolean finalActivo = activo == 1;
+
+                AbstractUserAdapter adapter = new AbstractUserAdapter(_session, realm, _model)
+                {
                     @Override
-                    public String getUsername() {
+                    public String getUsername()
+                    {
                         return finalUsername;
                     }
 
                     @Override
-                    public SubjectCredentialManager credentialManager() {
-                        return session.users().getUserCredentialManager(this);
+                    public String getFirstName()
+                    {
+                        return finalNombres;
                     }
+
+                    @Override
+                    public String getLastName()
+                    {
+                        return finalApellidos;
+                    }
+
+                    @Override
+                    public String getEmail()
+                    {
+                        return finalCorreo;
+                    }
+
+                    @Override
+                    public boolean isEmailVerified()
+                    {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean isEnabled()
+                    {
+                        return finalActivo;
+                    }
+
+                    @Override
+                    public SubjectCredentialManager credentialManager()
+                    {
+                        return _session.users().getUserCredentialManager(this);
+                    }
+
+                    @Override
+                    public String getFirstAttribute(String name)
+                    {
+                        return switch (name)
+                        {
+                            case UserModel.EMAIL -> finalCorreo;
+                            case UserModel.FIRST_NAME -> finalNombres;
+                            case UserModel.LAST_NAME -> finalApellidos;
+                            default -> null;
+                        };
+                    }
+
+                    @Override
+                    public Map<String, List<String>> getAttributes()
+                    {
+                        Map<String, List<String>> attributes = new HashMap<>();
+
+                        attributes.put(UserModel.USERNAME, List.of(finalUsername));
+                        attributes.put(UserModel.EMAIL, List.of(finalCorreo));
+                        attributes.put(UserModel.FIRST_NAME, List.of(finalNombres));
+                        attributes.put(UserModel.LAST_NAME, List.of(finalApellidos));
+
+                        return attributes;
+                    }
+
                 };
 
-                adapter.setSingleAttribute(UserModel.USERNAME, finalUsername);
+                //adapter.setSingleAttribute(UserModel.USERNAME, finalUsername);
 
                 return adapter;
-            }else {
+            }else
+            {
                 System.out.println("!!! LA DB NO DEVOLVIÓ NADA PARA: " + username);
             }
 
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
             System.err.println("!!! ERROR DE SQL EN EL PLUGIN:");
             e.printStackTrace();
         }
@@ -102,61 +183,69 @@ public class CustomUserStorageProvider
     }
 
     @Override
-    public UserModel getUserByEmail(
-            RealmModel realm,
-            String email) {
-
+    public UserModel getUserByEmail(RealmModel realm, String email)
+    {
         return null;
     }
 
     @Override
-    public boolean supportsCredentialType(String credentialType) {
-
+    public boolean supportsCredentialType(String credentialType)
+    {
         return PasswordCredentialModel.TYPE.equals(credentialType);
     }
 
     @Override
-    public boolean isConfiguredFor(
-            RealmModel realm,
-            UserModel user,
-            String credentialType) {
-
+    public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType)
+    {
         return supportsCredentialType(credentialType);
     }
 
     @Override
-    public boolean isValid(
-            RealmModel realm,
-            UserModel user,
-            CredentialInput input) {
+    public boolean isValid(RealmModel realm, UserModel user, CredentialInput input)
+    {
 
-        if (!supportsCredentialType(input.getType())) {
+        try
+        {
+            Class.forName("oracle.jdbc.OracleDriver");
+        }
+        catch (Exception e)
+        {
+            System.out.println("FALLA EN CLASS FORNAME EN ISVALID");
+            e.printStackTrace();
+        }
+
+        if (!supportsCredentialType(input.getType()))
+        {
+            System.out.println("FALLO EL SUPPORT");
             return false;
         }
 
-        try (Connection connection = DriverManager.getConnection(
-                "jdbc:oracle:thin:@192.168.1.4:1521/XE",
-                "SYSTEM",
-                "Greg1234.Database")) {
+        System.out.println("DB_URL: " + _db_url);
+        System.out.println("DB_USER: " + _db_username);
+        System.out.println("INTENTANDO CONEXION...");
 
+        try (Connection connection = DriverManager.getConnection(_db_url, _db_username, _db_password))
+        {
             PreparedStatement statement = connection.prepareStatement(
-                    "SELECT password FROM EXTRANET.USUARIOS WHERE username = ?"
+                    "SELECT contrasenia FROM user1.sso_users WHERE username = ?"
             );
 
             statement.setString(1, user.getUsername());
 
             ResultSet resultSet = statement.executeQuery();
 
-            if (resultSet.next()) {
-
-                String dbPassword = resultSet.getString("password");
+            if (resultSet.next())
+            {
+                String dbPassword = resultSet.getString("contrasenia");
 
                 return dbPassword.equals(
                         input.getChallengeResponse()
                 );
             }
 
-        } catch (SQLException e) {
+        } catch (SQLException e)
+        {
+            System.out.println("FALLO LA CONECCION");
             e.printStackTrace();
         }
 
--- src/main/java/gg/renz/CustomUserStorageProviderFactory.java
+++ src/main/java/gg/renz/CustomUserStorageProviderFactory.java
@@ -4,19 +4,17 @@ import org.keycloak.component.ComponentModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.storage.UserStorageProviderFactory;
 
-public class CustomUserStorageProviderFactory
-        implements UserStorageProviderFactory<CustomUserStorageProvider> {
-
+public class CustomUserStorageProviderFactory implements UserStorageProviderFactory<CustomUserStorageProvider>
+{
     @Override
-    public String getId() {
-        return "oracle-storage-gg";
+    public String getId()
+    {
+        return "oracle-storage-user1";
     }
 
     @Override
-    public CustomUserStorageProvider create(
-            KeycloakSession session,
-            ComponentModel model) {
-
+    public CustomUserStorageProvider create(KeycloakSession session, ComponentModel model)
+    {
         return new CustomUserStorageProvider(session, model);
     }
 }
